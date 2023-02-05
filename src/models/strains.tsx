3f144449a94bdff8f72388a961ef0b5456a2c2b4
wadebooth <wademgmt@gmail.com>
2023-02-04 21:26:45 -0500

    added interface for StrainData

--- src/models/strains.tsx
+++ src/models/strains.tsx
@@ -1,4 +1,23 @@
 export interface StrainData {
   id: string
   strain: string
+  thc: string
+  cbd: string
+  cbg: string
+  strainType: string
+  climate: string
+  difficulty: string
+  fungalResistance: null
+  indoorYieldInGramsMax: number
+  outdoorYieldInGramsMax: number
+  floweringWeeksMin: number
+  floweringWeeksMax: number
+  heightInInchesMin: number
+  heightInInchesMax: number
+  goodEffects: string
+  sideEffects: string
+  imgThumb: string
+  imgAttribution: null
+  imgAttributionLink: null
+  imgCreativeCommons: boolean
 }
